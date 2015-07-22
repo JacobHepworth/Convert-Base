96801b26b7e90cd09d2f169b308084a48c1e8f32
Jacob Hepworth <cinemacookie@gmail.com>
2015-07-22 17:19:38 -0600

    removed unneeded import

--- convertbase.py
+++ convertbase.py
@@ -1,6 +1,5 @@
 def ten_to_base(x, new_base,extend=[]):
     """Converts Base10 to BaseX(defined by user). Adding of new bases past Base36 is possible using the extend argument. Each new list item will be the new symbol for that base starting at Base37."""
-    import sys
     base_chart={10:'a',11:'b',12:'c',13:'d',14:'e',15:'f',16:'g',17:'h',18:'i',19:'j',20:'k',21:'l',22:'m',23:'n',24:'o',25:'p',26:'q',27:'r',28:'s',29:'t',30:'u',31:'v',32:'w',33:'x',34:'y',35:'z'}
     if extend:
         for i in range(len(extend)):
@@ -30,3 +29,8 @@ def convert_base(x, original_base, new_base=10):
         return ten_to_base(int(str(x)), new_base)
     else:
         return x
+for i in range(1,1000):
+    print(convert_base(i,10,35),
+    convert_base(i,10,10),
+    convert_base(convert_base(i,10,35),35,10),
+    convert_base(convert_base(i,10,16),16,2))
