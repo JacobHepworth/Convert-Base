677b15e97e98709ee8321e89c4811cea11252237
Jacob Hepworth <cinemacookie@gmail.com>
2015-07-22 05:12:56 -0600

    Python3 support added, Base10 to BaseX extension added, Readme.md updated

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Convert-Base
-Python 2 script to convert any base to any base. Only base 2-36 supported due to base 37 and greater requiring special characters.
+Python script to convert any base to any base. the function ten_to_base() can be extended to any base but symbols for each new base must be provided by the use of the user. Support for extention on BaseX to Base10 conversion is limited due to the need for the special characters incompatibily with pythons int() function. This is a future target and will be supported in a later version.
 
-To use do as follows:
-convert_base(x,original_base,new_base)
+###To use do as follows:
+>convert_base(x,original_base,new_base)
+>ten_to_base(x,new_base,extend=['!','@','#'])
+
+Note, the list under the extend argument is completely up to the user to define.
--- convertbase.py
+++ convertbase.py
@@ -1,28 +1,39 @@
-def _tentobase(x, new_base):
+def ten_to_base(x, new_base,extend=[]):
+    """Converts Base10 to BaseX(defined by user). Adding of new bases past Base36 is possible using the extend argument. Each new list item will be the new symbol for that base starting at Base37."""
+    import sys
     base_chart={10:'a',11:'b',12:'c',13:'d',14:'e',15:'f',16:'g',17:'h',18:'i',19:'j',20:'k',21:'l',22:'m',23:'n',24:'o',25:'p',26:'q',27:'r',28:'s',29:'t',30:'u',31:'v',32:'w',33:'x',34:'y',35:'z'}
-    out=''
+    if not extend:
+        pass
+    else:
+        for i in range(len(extend)):
+            base_chart[36+i] = extend[i]
+    out = ''
     current = x
     while current is not 0:
-        remain = current % new_base
-        if 36>remain>9:
+        remain = current%new_base
+        if new_base > 36 and not extend:
+            raise ValueError("Can't exceed base 36 without defining new bases")
+        elif remain > 9:
             remain_str=base_chart[remain]
-        elif remain>=36:
-            remain_str='('+str(remain)+')'
+        elif remain is 0:
+            return '0'
         else:
             remain_str=str(remain)
         out = remain_str+out
-        current=current/new_base
-    return out
+        if int(sys.version[0]) == 2:
+            current=current/new_base
+        elif int(sys.version[0]) >= 3:
+            current=current//new_base
+        else:
+            raise SystemError('This version of Python is not supported')
+    return str(out)
 def convert_base(x, original_base, new_base=10):
-    """Converts any base from 2-36
-    Default new base is set to 10"""
+    """Converts any Base to any Base but is limited to Base2 through Base36. Default new base is set to 10. Extending to a new base is not yet available with this function."""
     if original_base is not 10 and new_base is not 10:
-        return _tentobase(int(str(x),original_base),new_base)
+        return ten_to_base(int(str(x),original_base),new_base)
     elif original_base is not 10 and new_base is 10:
         return int(str(x),original_base)
     elif original_base is 10 and new_base is not 10:
-        return _tentobase(int(str(x)), new_base)
-    elif original_base is 10 and new_base is 10:
-        return x
+        return ten_to_base(int(str(x)), new_base)
     else:
-        raise ValueError('You done messed up something!')
+        return x
